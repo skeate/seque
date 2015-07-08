60d84bcd071a4939056d2daaea54b03109c1a15c
Jonathan Skeate <skeate@gmail.com>
2015-07-07 22:30:22 -0400

    Add mocha & karma into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var umd = require('gulp-umd');
 var karma = require('karma').server;
+var mocha = require('gulp-mocha');
+require('coffee-script/register');
 
 gulp.task('build', function() {
   gulp.src('src/**/*.js')
@@ -18,4 +20,18 @@ gulp.task('tdd', function(done) {
   }, done);
 });
 
+gulp.task('mocha', function() {
+  gulp.src('./test/seque_test.coffee', {read: false})
+    .pipe(mocha());
+});
+
+gulp.task('karma-ci', function(done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
+});
+
+// note: this does not actually seem to work for various reasons :/
+gulp.task('test', ['mocha', 'karma-ci']);
 gulp.task('default', ['build', 'tdd']);
